c52eaa3abcdce26845cea280cc2db145bce95481
Tiago Silva <programtiago@gmail.com>
2022-06-06 21:03:22 +0100

    Project initialized

---
+++ backend/src/test/java/com/devtiago/logisticsystem/LogisticSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devtiago.logisticsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogisticSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
